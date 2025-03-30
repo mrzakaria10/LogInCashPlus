59cf3df3fc92177dd207722b19ad7bcb4f35c3b4
ZAKARIA DERKAOUI <zakariadrk00@gmail.com>
2025-03-30 08:03:37 +0200

    create variable in file user.java

--- LoginCashPlus/src/main/java/com/logincashplus/models/User.java
+++ LoginCashPlus/src/main/java/com/logincashplus/models/User.java
@@ -1,18 +1,21 @@
 package com.logincashplus.models;
 
 public class User {
+	private int id;
 	private String username;
-	  private String hashedPassword;
-	public String getHashedPassword() {
-		return hashedPassword;
-	}
-	public void setHashedPassword(String hashedPassword) {
-		this.hashedPassword = hashedPassword;
-	}
-	public String getUsername() {
-		return username;
-	}
-	public void setUsername(String username) {
-		this.username = username;
-	}
+	  private String password;
+
+	  public User(int id, String username, String password) {
+		  this.id = id;
+		  this.username = username;
+		  this.password = password;
+	  }
+	  public int getId() {return id; }
+	  public void setId(int id) {this.id = id;}
+	  
+	  public String getUsername() {return username;}
+	  public void setUsername(String username) {this.username = username;}
+	  
+	  public String getPassword() {return password;}
+	  public void setPassword(String password) {this.password = password;}
 }
